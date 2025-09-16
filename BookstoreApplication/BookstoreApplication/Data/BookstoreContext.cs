9fa342938129a9b379f719d8a7e85396deee9257
AnaRadmanovic22 <anaradmanovic21@gmail.com>
2025-09-16 12:55:36 +0200

    Potrebna konfiguracija rešena, Migracije ispravno definisane

---
+++ BookstoreApplication/BookstoreApplication/Data/BookstoreContext.cs
@@ -0,0 +1,14 @@
+﻿using BookstoreApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreApplication.Data
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options) { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+}
